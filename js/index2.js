6d1b26dea8313cc047df84811dcc01194226de59
Prateek <mauryaprateek9451@gmail.com>
2024-10-15 12:59:07 +0530

    in2

--- js/index2.js
+++ js/index2.js
@@ -2,6 +2,16 @@ const Person={
  name:"xyz" ,
  age:30 ,
 }
-const newPerson=Person;
+const newPerson={...Person,name:"BUI"};
+console.log(Person);
+newPerson.age=35;
 console.log(Person);
 console.log(newPerson);
+
+
+const emp=["pra",35,10001.50];
+const emp1=[...emp];
+const emp2=[...emp,"GZB"];
+console.log(emp);
+console.log(emp1);
+console.log(emp2);
